5f3cbee2f559e6f908c9ad84176757a3948e6438
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-03-25 21:24:13 -0300

    Gerado Id Ong

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const crypto = require('crypto');
 
 const routes = express.Router();
 
@@ -6,7 +7,10 @@ routes.post('/ongs', (req, res) => {
   const { name, email, whatsapp, city, uf } = req.body;
   const { body } = req
 
+  const id = crypto.randomBytes(4).toString('HEX');
+
   console.table(body);
+  console.log(id)
   return res.json({ ok: true });
 });
 
