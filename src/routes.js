56143f6cc92152d3c356869c3ed98bf68e892476
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-03-27 23:25:05 -0300

    Lib celebrate e validação criação ong

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
@@ -10,7 +11,15 @@ const routes = express.Router();
 routes.post('/sessions', SessionController.store);
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.store);
+routes.post('/ongs', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required().min(3),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.number().required().min(10).max(11),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2),
+  })
+}), OngController.store);
 
 routes.post('/incidents', IncidentController.store);
 routes.get('/incidents', IncidentController.index);
