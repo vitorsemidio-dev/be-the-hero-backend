af0a8c1fd24454eb2e27d67b4c777a3d5debbeb6
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-03-28 00:26:25 -0300

    Executando migrations antes do teste

--- tests/integration/ong.spec.js
+++ tests/integration/ong.spec.js
@@ -1,7 +1,13 @@
 const request = require('supertest');
+
 const app = require('../../src/app');
+const connection = require('../../src/database/connection');
 
 describe('ONG', () => {
+  beforeEach(async () => {
+    await connection.migrate.latest();
+  });
+
   it('should be able to create a new ONG', async () => {
     const response = await request(app).post('/ongs').send({
       name: "abababa",
